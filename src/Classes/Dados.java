68e355382964d775aa978b515e7f659e83d53ebe
paulo.ti <paulo.ti@PAULO-TI.chevroletnova.local>
2020-01-20 18:07:31 -0200

    Commit Inicial do Projeto

---
+++ src/Classes/Dados.java
@@ -0,0 +1,13 @@
+
+package Classes;
+
+public class Dados {
+    public boolean validaUsuarios(String usuario, String senha){
+        if (usuario.equals("ti") && senha.equals("875348")){
+            return true;
+        }else{
+            return false;
+        }
+    }
+    
+}
---
+++ src/Classes/JavaProjeto.java
@@ -0,0 +1,22 @@
+
+package Classes;
+
+import Formularios.frmLogin;
+import Formularios.frmMenu;
+import Formularios.frmusuarios;
+
+
+public class JavaProjeto {
+
+    
+    public static void main(String[] args) {
+       
+        //tela de login
+        //frmLogin Login = new frmLogin();
+        //Login.setVisible(true);
+        //tela de menu para teste
+        frmMenu Usuarios = new frmMenu();
+        Usuarios.setVisible(true);
+    }
+    
+}
